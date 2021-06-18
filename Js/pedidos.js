c3e98974997242767448c42fdf63dc599ebcb855
Diego Trujillo <diegotrujillo0210@gmail.com>
2021-06-18 09:33:05 -0500

    Solucionando problema de los input

--- Js/pedidos.js
+++ Js/pedidos.js
@@ -1,3 +1,4 @@
+// ****************rubia*****************************
 function incrementar() {
     valor = document.getElementById("item");
     if (valor.value < 100) valor.value++;
@@ -15,4 +16,39 @@ function decrementar() {
 
 function makeAnOrder() {
     document.write("<h2>Su pedido se está en curso...</h2>");
+}
+
+
+// *************Morena********************
+
+function incrementarr() {
+    valor = document.getElementById("itemm");
+    if (valor.value < 100) valor.value++;
+}
+
+function miFunc() {
+    valor = document.getElementById("itemm");
+    if (valor.value < 100) valor.value++;
+}
+
+function decrementarr() {
+    valor = document.getElementById("itemm");
+    if (valor.value > 01) valor.value--;
+}
+
+// *************CLARA********************
+function incrementars() {
+    valor = document.getElementById("items");
+    if (valor.value < 100) valor.value++;
+}
+
+function miFunc() {
+    valor = document.getElementById("items");
+    if (valor.value < 100) valor.value++;
+}
+
+function decrementars() {
+    valor = document.getElementById("items ");
+    4
+    if (valor.value > 01) valor.value--;
 }
