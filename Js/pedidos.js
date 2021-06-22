9799db7b6d0d1767e0218d9c06d847beda0a428d
Diego Trujillo <diegotrujillo0210@gmail.com>
2021-06-22 10:50:11 -0500

    Diego

--- Js/pedidos.js
+++ Js/pedidos.js
@@ -90,12 +90,10 @@ function restaCervezas() {
 
 }
 
-// ********************************************************
+// <!-- ======================================== SECTION PORTADA ============================================ -->
 
-const expresiones = {
-    usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
-    nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-    password: /^.{4,12}$/, // 4 a 12 digitos.
-    correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-    telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+window.onload = function() {
+    setTimeout(function() {
+        document.getElementById('hero-background').style.width = '100%';
+    })
 }
